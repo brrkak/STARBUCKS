3490685d266cb114521d9da9a5edf84ef5d891c7
vancouver <kjh03291@gmail.com>
2023-09-14 19:19:59 +0900

    final

--- dist/main.fb6bbcaf.js
+++ dist/main.fb6bbcaf.js
@@ -132,6 +132,7 @@ searchInputEl.addEventListener("blur", function () {
   searchInputEl.setAttribute("placeholder", "");
 });
 var badgeEl = document.querySelector("header .badges");
+var toTopEl = document.querySelector("#to-top");
 window.addEventListener("scroll", _.throttle(function () {
   if (window.scrollY > 500) {
     //배지 숨기기
@@ -139,15 +140,125 @@ window.addEventListener("scroll", _.throttle(function () {
       opacity: 0,
       display: "none"
     });
+    // 버튼 보이기
+    gsap.to(toTopEl, 0.2, {
+      x: 0
+    });
   } else {
     // 배지 보이기
     gsap.to(badgeEl, 0.6, {
       opacity: 1,
       display: "block"
     });
+    // 버튼 숨기기
+    gsap.to(toTopEl, 0.2, {
+      x: 100
+    });
   }
 }, 300));
+toTopEl.addEventListener("click", function () {
+  gsap.to(window, 0.7, {
+    scrollTo: 0
+  });
+});
+
 // _.throttle(함수, 시간)
+var fadeEls = document.querySelectorAll(".visual .fade-in");
+fadeEls.forEach(function (fadeEl, index) {
+  gsap.to(fadeEl, 1, {
+    delay: (index + 1) * 0.7,
+    opacity: 1
+  });
+});
+
+// SWIPER
+
+// new Swiper(선택자, 옵션)
+new Swiper(".notice-line .swiper", {
+  direction: "vertical",
+  autoplay: true,
+  loop: true
+});
+new Swiper(".promotion .swiper", {
+  slidesPerView: 3,
+  // 한번에 보여줄 슬라이드 개수
+  spaceBetween: 10,
+  // 슬라이드 사이 여백
+  centeredSlides: true,
+  // 1번 슬라이드가 가운데 보이기
+  loop: true,
+  autoplay: {
+    delay: 5000
+  },
+  pagination: {
+    el: ".promotion .swiper-pagination",
+    // 페이지 번호 요소 선택자
+    clickable: true // 사용자의 페이지 번호 요소 제어 가능 여부
+  },
+
+  navigation: {
+    prevEl: ".promotion .swiper-prev",
+    nextEl: ".promotion .swiper-next"
+  }
+});
+new Swiper(".awards .swiper", {
+  autoplay: true,
+  loop: true,
+  spaceBetween: 30,
+  slidesPerView: 5,
+  navigation: {
+    prevEl: ".awards .swiper-prev",
+    nextEl: ".awards .swiper-next"
+  }
+});
+var promotionEl = document.querySelector(".promotion");
+var promotionToggleBtn = document.querySelector(".toggle-promotion");
+var isHidePromotion = false;
+promotionToggleBtn.addEventListener("click", function () {
+  isHidePromotion = !isHidePromotion;
+  if (isHidePromotion) {
+    // 숨김 처리
+    promotionEl.classList.add("hide");
+  } else {
+    // 보임 처리
+    promotionEl.classList.remove("hide");
+  }
+});
+
+// 범위 랜덤 함수(소수점 2자리까지)
+function random(min, max) {
+  // `.toFixed()`를 통해 반환된 문자 데이터를,
+  // `parseFloat()`을 통해 소수점을 가지는 숫자 데이터로 변환
+  return parseFloat((Math.random() * (max - min) + min).toFixed(2));
+}
+function floatingObject(selecter, delay, size) {
+  //gsap.to(요소,시간,옵션)
+  gsap.to(selecter,
+  // 선택자
+  random(1.5, 2.5),
+  // 애니메이션 동작 시간
+  {
+    //옵션
+    y: size,
+    repeat: -1,
+    yoyo: true,
+    ease: Power1.easInOut,
+    delay: random(0, delay)
+  });
+}
+floatingObject(".floating1", 1, 15);
+floatingObject(".floating2", 0.5, 15);
+floatingObject(".floating3", 1.5, 20);
+var spyEls = document.querySelectorAll("section.scroll-spy");
+spyEls.forEach(function (spyEl) {
+  new ScrollMagic.Scene({
+    triggerElement: spyEl,
+    // 보여짐 여부를 감시할 요소를 지정
+    tirggerHook: 0.8
+  }).setClassToggle(spyEl, "show").addTo(new ScrollMagic.Controller());
+});
+var thisYear = document.querySelector(".this-year");
+thisYear.textContent = new Date().getFullYear(); // 현재 년도
 },{}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -173,7 +284,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55803" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50220" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
